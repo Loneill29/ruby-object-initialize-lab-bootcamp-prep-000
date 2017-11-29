4f07d08c2f615189b766830eeab3623fb3d3453d
Linda ONeill <oneill.linda21@gmail.com>
2017-11-29 22:31:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,5 @@
 class Dog
-  def initialize(name)
-    @name = name
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-
-  def initialize(breed)
-    @breed = "Mutt"
-  end
-end
